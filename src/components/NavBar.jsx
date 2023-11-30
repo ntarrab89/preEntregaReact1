7be13e4cb6e9a40d5bc3bec76564d5256e652236
terra2909 <tarrabnicolas@gmail.com>
2023-11-29 21:02:41 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+const NavBar=()=>{
+    return(<header className="header">
+<img src='https://images.vexels.com/media/users/3/200097/isolated/preview/942820836246f08c2d6be20a45a84139-icono-de-carrito-de-compras-carrito-de-compras.png' className="chart"></img>
+<h1 className="header__title">Navbar</h1>
+<nav className="navBar">
+    <a href ='' className="header__link">link 1</a>
+    <a href ='' className="header__link">link 2</a>
+    <a href ='' className="header__link">link 3</a>
+</nav>
+</header>
+)
+}
+export default NavBar
